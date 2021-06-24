0bc72bf178ce3cc1d4257369b4f3b61e40245de1
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2021-06-24 13:48:53 +0200

    Le formulaire php

--- index.php
+++ index.php
@@ -26,8 +26,23 @@ if (2  == $var) {
 
 */
 ?>
+
 <?php require __DIR__ . '/class/resolver.php'; ?>
+<?php var_dump($_GET); ?><br><br><br>
+<?php var_dump($_POST); ?>
 <?php require (new resolver(__DIR__, 'inc', 'header'))->getPath() ?>
 
-<p>llllllllllllllllllllllllllllllllllllllllllllllllll</p>
+<form>
+    <input name="name" placeholder="Un nom svp" /><br><br>
+    <input name="age" placeholder="Un age svp" /><br><br>
+    <input name="pat" placeholder="ce que vous voulez" /><br><br>
+    <button type="submit">Soumettre</button>
+</form>
+
+<p>Compressing objects: 100% (12/12), done.
+    Writing objects: 100% (14/14), 1.85 KiB | 379.00 KiB/s, done.
+    Total 14 (delta 0), reused 0 (delta 0), pack-reused 0
+    To https://github.com/abakarabdelsalam/R-voir-Cours-PHP.git
+    * [new branch] master -> master
+    PS C:\xampp\htdocs\R-voir-Cours-PHP\R-voir-Cours-PHP></p>
 <?php require (new resolver(__DIR__, 'inc', 'footer'))->getPath() ?>
